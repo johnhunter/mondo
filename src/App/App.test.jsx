bd125cb507c7dc32fe50b29031822098207d60ce
John Hunter <john.hunter@apperio.com>
2022-10-15 19:50:38 +0100

    WIP add jest and testing-library

---
+++ src/App/App.test.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+
+import App from "./App";
+
+describe("App", () => {
+  test("renders App component", () => {
+    render(<App />);
+
+    expect(screen.getByText("App")).toBeInTheDocument();
+  });
+});
