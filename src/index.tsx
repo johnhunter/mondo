82d8354ad3b410eb9e8418bf2978f187f170e6b4
John Hunter <john.hunter@apperio.com>
2022-10-23 14:50:40 +0100

    Convert to TS

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.module.css';
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById('root') as HTMLElement;
 ReactDOM.createRoot(rootElement).render(
   <StrictMode>
     <App />
