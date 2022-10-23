9054baa06aeefbf2c59ecc83de1ec9ec4a814f8f
John Hunter <john.hunter@apperio.com>
2022-10-23 14:28:03 +0100

    Add tests for post navigation
    - Reformatted css file
    - Add test for post flow

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -7,11 +7,32 @@ test.describe('Home page', () => {
     await expect(page).toHaveTitle(/^Mondo$/);
   });
 
-  test('has the App heading', async ({ page }) => {
+  test('has the App structure', async ({ page }) => {
     await page.goto('/');
 
     await expect(page.getByRole('heading', { name: 'Posts' })).toBeInViewport();
+    await expect(page.getByRole('main')).toBeInViewport();
   });
 
-  // TODO: more tests and add mocking
+  test('has a link to a post', async ({ page }) => {
+    // TODO: use mocked data rather than external API
+    const name =
+      'sunt aut facere repellat provident occaecati excepturi optio reprehenderit';
+    const length = 100;
+
+    await page.goto('/');
+
+    const list = page.getByRole('list');
+    const thePost = list.getByRole('link', {
+      name,
+    });
+
+    await expect(list.getByRole('link')).toHaveCount(length);
+    await expect(thePost).toBeInViewport();
+
+    await thePost.click();
+    await expect(
+      page.getByRole('heading', { level: 1, name }),
+    ).toBeInViewport();
+  });
 });
