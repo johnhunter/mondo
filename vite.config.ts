0c3b776fc2e9b304f1900acc0b8ccb43c409769d
John Hunter <john.hunter@apperio.com>
2022-10-29 17:17:52 +0100

    Add path aliasing

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@
 
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   test: {
     globals: true,
     environment: 'jsdom',
