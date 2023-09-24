2373233af09f0d227a967509a41de6854efc4839
John Hunter <john@johnhunter.info>
2023-09-24 17:16:32 +0100

    Add and update tests

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,5 +1,9 @@
 import css from './App.module.css';
 
-const App = () => <div className={css.container}>App</div>;
+const App = () => (
+  <div data-testid="app" className={css.container}>
+    App
+  </div>
+);
 
 export default App;
